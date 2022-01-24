6e9da049d61d99c69271e27d40062bb51501569d
khairul111010 <khairul18103199@gmail.com>
2022-01-25 02:26:06 +0600

    middleware error handling

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -13,7 +13,7 @@ const ProductScreen = () => {
   useEffect(() => {
     const fetchProduct = async () => {
       const { data } = await axios.get(`/api/products/${id}`);
-      setProduct(data[0]);
+      setProduct(data);
     };
     fetchProduct();
   }, [id]);
