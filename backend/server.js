e5991c0980277c8ff020e303863c6249d1d58147
khairul111010 <khairul18103199@gmail.com>
2022-01-25 01:47:54 +0600

    product Routes

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
-import products from "./Data/products.js";
+import productRoutes from "./routes/productRoutes.js";
+
 dotenv.config();
 
 connectDB();
@@ -11,13 +12,9 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("API running");
 });
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-app.get("/api/products/:id", (req, res) => {
-  const product = products.filter((p) => p._id === req.params.id);
-  res.json(product);
-});
+
+app.use("/api/products", productRoutes);
+
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
