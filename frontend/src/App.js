eee423b78e56117ee21cb1a642dc46d5e6e38f61
khairul111010 <khairul18103199@gmail.com>
2022-01-26 22:52:29 +0600

    user Login form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import LoginScreen from "./screens/LoginScreen";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CartScreen from "./screens/CartScreen";
 
@@ -14,6 +15,7 @@ const App = () => {
         <Container>
           <Routes>
             <Route exact path="/" element={<HomeScreen />} />
+            <Route exact path="/login" element={<LoginScreen />} />
             <Route exact path="/product/:id" element={<ProductScreen />} />
             <Route exact path="/cart/:id" element={<CartScreen />} />
             <Route exact path="/cart" element={<CartScreen />} />
